a3cb85069c79012a2db5a7612917ff1f5e68d17d
freegroup4 <freegroup4@yopmail.com>
2014-05-25 08:13:50 +0800

    Final Commit
    Final

--- src/app11/App11.java
+++ src/app11/App11.java
@@ -1,6 +1,20 @@
 package app11;
 public class App11 {
     public static void main(String[] args) {
-        
+        int n;
+        n = star(8);
+        System.out.println( n + " star number");
+    }
+    public static int star(int n){
+        //印出2*n個星號並回傳
+        for (int i = 0; i < 2*n; i++) 
+            System.out.print("*");
+       System.out.println("\n");
+       return 2*n;
     }
 }
+
+
+
+
+
